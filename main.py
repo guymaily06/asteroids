d164585ad69777245468814d44047298441b7048
guymaily06 <guymaily06@gmail.com>
2025-05-18 00:18:35 +0300

    adding required command

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
     while x == 0 :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         pygame.Surface.fill (screen, "black")
         pygame.display.flip ()
 
