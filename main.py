1ad90daeab7c114668c762e43c6275711a4f6423
guymaily06 <guymaily06@gmail.com>
2025-05-18 00:12:03 +0300

    first try

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while SCREEN_HEIGHT == 720:
+        fill("black", rect = None, special_flags = 0)
+        pygame.display.flip ()
 
 if __name__ == "__main__":
     main()
