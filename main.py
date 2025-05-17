68424c9c66dd90dca2c42d132653c44834dcf3d7
guymaily06 <guymaily06@gmail.com>
2025-05-18 00:21:54 +0300

    Reflecting the main file to be more eloquenter

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import pygame
 from constants import *
 
 def main():
-    x = 0
-    print ("Starting Asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while x == 0 :
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill (screen, "black")
+        screen.fill ("black")
         pygame.display.flip ()
 
 if __name__ == "__main__":
