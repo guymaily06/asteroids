7450b77a1d9eda9c190ab03a4f480c017e642191
guymaily06 <guymaily06@gmail.com>
2025-05-18 00:36:17 +0300

    I did mean "Clock"

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.clock()
+    clock = pygame.time.Clock()
     dt = 0
     
     while True:
