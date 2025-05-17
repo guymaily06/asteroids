2883e45939e3a88e11299da0d3d76f9a71ad5347
guymaily06 <guymaily06@gmail.com>
2025-05-18 00:33:47 +0300

    fixing understanding of clock method

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
-    Clock() = Clock
-    pygame.time.Clock()
+    Clock = pygame.time.clock()
     dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill ("black")
         pygame.display.flip ()
-        Clock.tick(60)
-        dt = (Clock.tick()/1000)
+        
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
