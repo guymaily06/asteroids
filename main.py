4c811cfc5c959f1d386217b43297db73d377b795
guymaily06 <guymaily06@gmail.com>
2025-05-19 19:30:47 +0300

    adding character classes

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     dt = 0
     
     while True:
@@ -17,9 +23,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
         player.update (dt)
         screen.fill ("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         player.draw(screen)
+      
+      
         pygame.display.flip ()
 
         dt = clock.tick(60) / 1000
