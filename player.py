a3150396346a84233ad288bfc0ce38980770d8f0
guymaily06 <guymaily06@gmail.com>
2025-05-20 22:58:07 +0300

    removing extra underscore

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self._shoot_timer = 0
+        self.shoot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -45,8 +45,6 @@ class Player(CircleShape):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
     
-    
-
 
     def rotate (self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
